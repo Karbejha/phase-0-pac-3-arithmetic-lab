a2aa6d8415a93183b2f4a12dd5887b0a27433baa
Mohamad Karbejha <78583194+Karbejha@users.noreply.github.com>
2023-10-04 16:18:38 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+    return a + b;
+  }
+function subtract(a, b) {
+    return a - b;
+  }
+function multiply(a, b) {
+    return a * b;
+  }
+function add(a, b) {
+    a += b;
+    return a;
+  }
+  
+function subtract(a, b) {
+    a -= b;
+    return a;
+  }
+  
+function multiply(a, b) {
+    a *= b;
+    return a;
+  }
+function increment(n) {
+    return n + 1;
+  }
+function decrement(n) {
+    return n - 1;
+  }
+function makeInt(string) {
+    return parseInt(string, 10);
+  }
+function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+function divide(a, b) {
+    return a / b;
+  }
+  
